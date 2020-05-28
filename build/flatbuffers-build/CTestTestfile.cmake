b808607117be98d389956b25eafe1acea0b01c0f
adeilh <madeel234@yahoo.com>
2020-05-29 00:01:33 +0500

    Added .gitignore

--- build/flatbuffers-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mahussain/Logiciel/flexBuffersDemo/flatbuffers
-# Build directory: /home/mahussain/Logiciel/flexBuffersDemo/build/flatbuffers-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(flattests "/home/mahussain/Logiciel/flexBuffersDemo/build/flatbuffers-build/flattests")
-set_tests_properties(flattests PROPERTIES  _BACKTRACE_TRIPLES "/home/mahussain/Logiciel/flexBuffersDemo/flatbuffers/CMakeLists.txt;620;add_test;/home/mahussain/Logiciel/flexBuffersDemo/flatbuffers/CMakeLists.txt;0;")
