522a0900a65c1a2ea0cfeba93f99ece65325d146
adeilh <madeel234@yahoo.com>
2020-05-28 23:28:24 +0500

    Basic implementation of flexbuffers to send and receive data

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mahussain/Logiciel/flexBuffersDemo/server.cpp" "/home/mahussain/Logiciel/flexBuffersDemo/build/CMakeFiles/server.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../flatbuffers/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mahussain/Logiciel/flexBuffersDemo/build/flatbuffers-build/CMakeFiles/flatbuffers.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
